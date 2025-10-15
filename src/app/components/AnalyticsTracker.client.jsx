49d1adda0b1c1e8861e6127ba4cc24283d3d8c4f
Suwat Srichan <exadsdev@gmail.com>
2025-10-15 22:45:20 +0700

    Create AnalyticsTracker.client.jsx

---
+++ src/app/components/AnalyticsTracker.client.jsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function AnalyticsTracker() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const url = searchParams?.toString()
+      ? `${pathname}?${searchParams.toString()}`
+      : pathname;
+
+    if (typeof window !== 'undefined' && window.gtag && window.__GAID__) {
+      window.gtag('config', window.__GAID__, { page_path: url });
+    }
+  }, [pathname, searchParams]);
+
+  return null;
+}
